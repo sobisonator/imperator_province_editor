35c9437fd769d9bd8cc1fb599c172937394b8c59
sobisonator <38443890+sobisonator@users.noreply.github.com>
2019-07-14 20:52:51 +0100

    Upload DB schema

---
+++ province_setup_schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS province_setup (
+	ProvID INTEGER PRIMARY KEY,
+	Culture VARCHAR,
+	Religion VARCHAR,
+	TradeGoods VARCHAR,
+	Citizens INTEGER,
+	Freedmen INTEGER,
+	Slaves INTEGER,
+	Tribesmen INTEGER,
+	Civilization INTEGER,
+	Barbarian BOOLEAN,
+	NameRef VARCHAR,
+	AraRef VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS definition (
+	Province_id INTEGER PRIMARY KEY,
+	R INTEGER,
+	G INTEGER,
+	B INTEGER,
+	Name VARCHAR,
+	x VARCHAR
+);
